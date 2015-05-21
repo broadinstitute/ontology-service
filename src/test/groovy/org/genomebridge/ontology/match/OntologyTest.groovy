8a217ae6b49f4267da251245df0e25d2afc1e6c3
Greg Rushton <grushton@broadinstitute.org>
2015-05-21 09:43:48 -0400

    Add methods and aggregate research terms and tests.

--- src/test/groovy/org/genomebridge/ontology/match/OntologyTest.groovy
+++ src/test/groovy/org/genomebridge/ontology/match/OntologyTest.groovy
@@ -12,14 +12,12 @@ package org.genomebridge.ontology.match
 import groovy.transform.CompileStatic
 import org.genomebridge.ontology.match.api.Consent
 import org.genomebridge.ontology.match.api.ResearchPurpose
-import org.genomebridge.ontology.match.api.model.And
-import org.genomebridge.ontology.match.api.model.Named
-import org.genomebridge.ontology.match.api.model.UseRestriction
+import org.genomebridge.ontology.match.api.model.*
 import org.genomebridge.ontology.match.ontology.OntologyMatcher
 import org.junit.Test
 
-import static org.junit.Assert.assertTrue
 import static org.junit.Assert.assertFalse
+import static org.junit.Assert.assertTrue
 
 /**
  *
@@ -35,6 +33,76 @@ class OntologyTest {
     private static final String MOCK_OWNER = "mock owner"
     private static final String MOCK_RESEARCHER = "mock researcher"
     private static final OntologyMatcher ONTOLOGY = new OntologyMatcher()
+
+    @Test
+    void testMethodsResearch() {
+        def purpose = new ResearchPurpose(
+                id: UUID.randomUUID().toString(),
+                researcher: MOCK_RESEARCHER,
+                purpose: new Named(name: "http://www.genomebridge.org/ontologies/DURPO/methods_research")
+        )
+        def consent = new Consent(
+                id: UUID.randomUUID().toString(),
+                owner: MOCK_OWNER,
+                requiresManualReview: false,
+                restriction: new Everything()
+        )
+        def match = ONTOLOGY.match(purpose.purpose, consent.restriction)
+        assertTrue("We should have a match between consent and purpose", match)
+    }
+
+    @Test
+    void testMethodsResearchExcluded() {
+        def purpose = new ResearchPurpose(
+                id: UUID.randomUUID().toString(),
+                researcher: MOCK_RESEARCHER,
+                purpose: new Named(name: "http://www.genomebridge.org/ontologies/DURPO/methods_research")
+        )
+        def consent = new Consent(
+                id: UUID.randomUUID().toString(),
+                owner: MOCK_OWNER,
+                requiresManualReview: false,
+                restriction: new Not(
+                        operand: new Named(name: "http://www.genomebridge.org/ontologies/DURPO/methods_research"))
+        )
+        def match = ONTOLOGY.match(purpose.purpose, consent.restriction)
+        assertFalse("We should NOT have a match between consent and purpose", match)
+    }
+
+    @Test
+    void testAggregateResearch() {
+        def purpose = new ResearchPurpose(
+                id: UUID.randomUUID().toString(),
+                researcher: MOCK_RESEARCHER,
+                purpose: new Named(name: "http://www.genomebridge.org/ontologies/DURPO/aggregate_research")
+        )
+        def consent = new Consent(
+                id: UUID.randomUUID().toString(),
+                owner: MOCK_OWNER,
+                requiresManualReview: false,
+                restriction: new Everything()
+        )
+        def match = ONTOLOGY.match(purpose.purpose, consent.restriction)
+        assertTrue("We should have a match between consent and purpose", match)
+    }
+
+    @Test
+    void testAggregateResearchExcluded() {
+        def purpose = new ResearchPurpose(
+                id: UUID.randomUUID().toString(),
+                researcher: MOCK_RESEARCHER,
+                purpose: new Named(name: "http://www.genomebridge.org/ontologies/DURPO/aggregate_research")
+        )
+        def consent = new Consent(
+                id: UUID.randomUUID().toString(),
+                owner: MOCK_OWNER,
+                requiresManualReview: false,
+                restriction: new Not(
+                        operand: new Named(name: "http://www.genomebridge.org/ontologies/DURPO/aggregate_research"))
+        )
+        def match = ONTOLOGY.match(purpose.purpose, consent.restriction)
+        assertFalse("We should NOT have a match between consent and purpose", match)
+    }
 
     @Test
     void testMatchRestriction() {
