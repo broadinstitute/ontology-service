dc6a794dec02f118e75628af271452b471d90014
Greg Rushton <rushtong@users.noreply.github.com>
2021-01-19 13:38:36 -0500

    add micronaut framework (#2)

---
+++ src/test/java/org/broadinstitute/dsp/ontology/services/OntologyCommandTest.java
@@ -0,0 +1,28 @@
+package org.broadinstitute.dsp.ontology.services;
+
+import io.micronaut.configuration.picocli.PicocliRunner;
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.context.env.Environment;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class OntologyCommandTest {
+
+    @Test
+    public void testWithCommandLineOption() throws Exception {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
+
+        try (ApplicationContext ctx = ApplicationContext.run(Environment.CLI, Environment.TEST)) {
+            String[] args = new String[] { "-v" };
+            PicocliRunner.run(OntologyCommand.class, ctx, args);
+
+            // ontology
+            assertTrue(baos.toString().contains("Hi!"));
+        }
+    }
+}
